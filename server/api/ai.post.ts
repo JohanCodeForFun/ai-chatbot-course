408c957d76aed16ab87120c0f958731e869b22c7
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-12 15:40:36 +0200

    Update AI post endpoint to include customer support agent with training how the chat bot should and should not respond

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,4 +1,5 @@
 import { OpenAI } from "openai";
+import { customerSupportAgent } from "~/agents";
 
  export default defineEventHandler(async (event) => {
   const body = await readBody(event);
@@ -8,13 +9,14 @@ import { OpenAI } from "openai";
   //  const configuration = new Configuration({
   //    apiKey: OPENAI_API_KEY,
   //  });
-  
+
    const openai = new OpenAI();
 
    const completion = await openai.chat.completions.create({
      model: "gpt-3.5-turbo",
      messages: body.messages || [],
      temperature: body.temperature || 1,
+     ...customerSupportAgent(body)
    });
    return completion;
  });
