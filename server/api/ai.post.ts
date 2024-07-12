898bd92620d6b781903f7587a6d0323780e2f5e6
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-12 15:18:31 +0200

    Update AI post endpoint to use request body for messages and temperature

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,16 +1,20 @@
 import { OpenAI } from "openai";
 
  export default defineEventHandler(async (event) => {
-   const { OPENAI_API_KEY } = useRuntimeConfig();
+  const body = await readBody(event);
+
+  //  const { OPENAI_API_KEY } = useRuntimeConfig();
 
   //  const configuration = new Configuration({
   //    apiKey: OPENAI_API_KEY,
   //  });
+  
    const openai = new OpenAI();
 
    const completion = await openai.chat.completions.create({
      model: "gpt-3.5-turbo",
-     messages: [{ role: "user", content: "Hello world" }],
+     messages: body.messages || [],
+     temperature: body.temperature || 1,
    });
    return completion;
  });
